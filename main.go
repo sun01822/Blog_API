e2e11f9f376726e1ce19e30abbbe2f5787c9c030
MD. Shariar Hossain Sun <mdshariarhossainsun01822@gmail.com>
2024-10-07 22:06:20 +0600

    added middlewares

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	_ "Blog_API/docs"
 	"Blog_API/pkg/containers"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	m "github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
-	_ "Blog_API/docs"
+	"net/http"
 )
-
 
 // swagger docs
 // @title Blog API
@@ -22,11 +23,19 @@ import (
 // @host localhost:8080
 // @BasePath /blog_api/v1
 func main() {
-	e:= echo.New()
+	e := echo.New()
 	e.Use(middleware.CORS())
+	e.Pre(m.RemoveTrailingSlash())
+	e.Use(m.LoggerWithConfig(m.LoggerConfig{
+		Format:           `${time_custom} ${remote_ip} ${host} ${method} ${uri} ${status} ${latency_human} ${bytes_in} ${bytes_out} "${user_agent}"` + "\n",
+		CustomTimeFormat: "2006-01-02T15:04:05.00",
+	}))
+	e.Use(m.Secure())
+	e.Use(m.Recover())
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "I am alive!!!")
+	})
 	containers.Serve(e)
 }
-
-
