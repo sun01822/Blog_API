33f1cd9e199384949fd32618f132e8461ba83eee
MD. Shariar Hossain Sun <mdshariarhossainsun01822@gmail.com>
2024-11-05 23:38:19 +0600

    update swagger host path

--- docs/docs.go
+++ docs/docs.go
@@ -678,7 +678,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "192.168.10.135:8080",
+	Host:             "localhost:8080",
 	BasePath:         "/blog_api/v1",
 	Schemes:          []string{},
 	Title:            "Blog API",
