6fc2344dee8e2438cc74ae0fbacab2dd7aed7c50
MD. Shariar Hossain Sun <mdshariarhossainsun01822@gmail.com>
2024-11-05 15:21:41 +0600

    update swagger host

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 // @contact.email support@swagger.io
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8080
+// @host http://192.168.10.135:8080
 // @BasePath /blog_api/v1
 func main() {
 	e := echo.New()
